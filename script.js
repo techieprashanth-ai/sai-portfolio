f927b2a8587ffffbf6362c22a3dfa2c831094b8b
techieprashanth-ai <prashanth949.ai@gmail.com>
2026-06-13 02:47:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Sai Portfolio Loaded");
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if(entry.isIntersecting){
+      entry.target.classList.add("show");
+    }
+  });
+},{
+  threshold:0.15
+});
+
+sections.forEach((section)=>{
+  observer.observe(section);
+});

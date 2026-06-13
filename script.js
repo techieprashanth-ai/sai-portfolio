4db663e967e5541467c6337e6a0f2c091e682067
techieprashanth-ai <prashanth949.ai@gmail.com>
2026-06-13 11:44:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,14 @@ const observer = new IntersectionObserver((entries) => {
 sections.forEach((section)=>{
   observer.observe(section);
 });
+
+const counters = document.querySelectorAll("#stats h2");
+
+counters.forEach(counter => {
+  const target = counter.innerText;
+  counter.style.opacity = "0";
+  
+  setTimeout(() => {
+    counter.style.opacity = "1";
+  }, 500);
+});

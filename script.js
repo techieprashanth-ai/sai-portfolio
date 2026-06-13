67d7cdaeadf5921bb4cefcbf8b7172f46460a6d2
techieprashanth-ai <prashanth949.ai@gmail.com>
2026-06-13 23:51:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -121,3 +121,24 @@ heroImage.style.transform =
 }
 
 });
+
+/* SCROLL PROGRESS */
+
+window.addEventListener("scroll", () => {
+
+const winScroll =
+document.body.scrollTop ||
+document.documentElement.scrollTop;
+
+const height =
+document.documentElement.scrollHeight -
+document.documentElement.clientHeight;
+
+const scrolled =
+(winScroll / height) * 100;
+
+document.getElementById(
+"progress-bar"
+).style.width = scrolled + "%";
+
+});
